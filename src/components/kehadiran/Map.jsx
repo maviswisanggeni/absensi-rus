0de296304038b272871f0d62d1e374cbde4cbc31
Danar <DanarGdg>
2023-08-17 18:03:37 +0700

    fix: [DG] valid indicator and map center coordinate

--- src/components/kehadiran/Map.jsx
+++ src/components/kehadiran/Map.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Circle } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { useDispatch, useSelector } from 'react-redux';
+import Skeleton from 'react-loading-skeleton';
 
 const iconMarker = new L.Icon({
     iconUrl: require('../../assets/icons/marker-location.png'),
@@ -12,17 +14,25 @@ const iconMarker = new L.Icon({
 const redOptions = { color: 'red' }
 
 function Map({ latitude, longitude, loading }) {
+    const dispatch = useDispatch()
+    const { latitude: latitudeSekolah, longitude: longitudeSekolah, radius, loading: loadingGetKoordinat } = useSelector(state => state.koordinat)
+
     return (
         <div className='map'>
-            <MapContainer center={[-6.753622, 110.843356]} zoom={20} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Circle center={[-6.7535586, 110.843736]} pathOptions={redOptions} radius={100} />
-                <Marker position={loading ? [latitude, longitude] : [-6.7535586, 110.843736]} icon={iconMarker}>
-                </Marker>
-            </MapContainer>
+            {loadingGetKoordinat ? (
+                <Skeleton height={500} width={500} />
+            ) : (
+                <MapContainer center={[latitude, longitude]} zoom={20} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Circle center={[latitudeSekolah, longitudeSekolah]} pathOptions={redOptions} radius={radius} />
+                    <Marker position={!loading ? [latitude, longitude] : [latitudeSekolah, longitudeSekolah]} icon={iconMarker}>
+                    </Marker>
+                </MapContainer>
+            )
+            }
         </div>
     )
 }
