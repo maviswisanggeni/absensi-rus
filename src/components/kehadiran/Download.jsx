425bd10c8dd67c91e106c2868071bfe5a1177ab4
DanarGdg <danargading@gmail.com>
2024-01-04 10:21:57 +0700

    fix export kehadiran

--- src/components/kehadiran/Download.jsx
+++ src/components/kehadiran/Download.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import donwload from '../../assets/icons/download-grey.svg' 
+import donwload from '../../assets/icons/download-grey.svg'
 import { useTanggalKehadiran } from '../../contexts/app/ContextTanggalKehadiran'
+import { useSelector } from 'react-redux'
 
 function Download() {
   const context = useTanggalKehadiran()
-  function endTimeCondition(){
-    if(context.endText === 'Tanggal berakhir'){
+  const { startTime, endTime, endText } = useSelector(state => state.kehadiran)
+
+  function endTimeCondition() {
+    if (endText === 'Tanggal berakhir') {
       return ''
-    }else{
-      return `&end_time=${context.endTime}`
+    } else {
+      return `&end_time=${endTime}`
     }
   }
-  function downloadExcel(){
-    window.location.replace(`https://absensiguru.smkrus.com/api/dashboard/donload?start_time=${context.startTime}${endTimeCondition()}`)
+  function downloadExcel() {
+    window.location.replace(`https://absensiguru.smkrus.com/api/dashboard/donload?start_time=${startTime}${endTimeCondition()}`)
   }
   return (
     <div>
-        <img src={donwload} alt="" onClick={downloadExcel}/>
+      <img src={donwload} alt="" onClick={downloadExcel} />
     </div>
   )
 }
